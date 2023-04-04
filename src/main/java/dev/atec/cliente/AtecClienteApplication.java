973da35fa5abb9946ee9c4c3bb902bbd7c473f76
e-naldo <88638321+e-naldo@users.noreply.github.com>
2023-04-04 14:00:47 -0300

    Initial commit

---
+++ src/main/java/dev/atec/cliente/AtecClienteApplication.java
@@ -0,0 +1,13 @@
+package dev.atec.cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtecClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtecClienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/atec/cliente/AtecClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.atec.cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtecClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
