64780de70b62ef18dc774d94f00a629e1bbdeb87
e-naldo <88638321+e-naldo@users.noreply.github.com>
2023-04-07 15:42:52 -0300

    Update ClienteController

--- src/main/java/dev/atec/cliente/controller/ClienteController.java
+++ src/main/java/dev/atec/cliente/controller/ClienteController.java
@@ -6,11 +6,14 @@ import dev.atec.cliente.dto.ClienteDetalhesDto;
 import dev.atec.cliente.entity.Cliente;
 import dev.atec.cliente.mapper.ClienteMapper;
 import dev.atec.cliente.service.ClienteService;
+import jakarta.validation.Valid;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/clientes")
@@ -25,7 +28,7 @@ public class ClienteController {
     }
 
     @PostMapping
-    public ResponseEntity<ClienteDetalhesDto> cadastrar(ClienteCadastroDto dto){
+    public ResponseEntity<ClienteDetalhesDto> cadastrar(@RequestBody @Valid ClienteCadastroDto dto){
         Cliente cliente = service.cadastrar(mapper.toEntity(dto));
 
         UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.newInstance();
@@ -35,7 +38,7 @@ public class ClienteController {
     }
 
     @PutMapping
-    public ResponseEntity<ClienteDetalhesDto> atualizar(ClienteAtualizacaoDto dto){
+    public ResponseEntity<ClienteDetalhesDto> atualizar(@RequestBody @Valid ClienteAtualizacaoDto dto){
         Cliente cliente = service.atualizar(mapper.toEntity(dto));
 
         return ResponseEntity.ok(mapper.toDetalhesDto(cliente));
@@ -46,6 +49,13 @@ public class ClienteController {
         Cliente cliente = service.buscarPorId(id);
 
         return ResponseEntity.ok(mapper.toDetalhesDto(cliente));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ClienteDetalhesDto>> listarTodos(){
+        List<Cliente> clientes = service.buscarTodos();
+
+        return ResponseEntity.ok(mapper.toClienteDetalhesDtos(clientes));
     }
 
     @DeleteMapping("{id}")
